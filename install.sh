ef21d7fed1d2f083ed7ab70a1ef2855a2f13607b
Zhansong Li <lizhansong@hvariant.com>
2019-10-26 15:01:50 +1100

    stow: gitconfig

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-stow i3
-stow nvim
-stow vim
-stow zshrc
+stow i3 -t ~
+stow nvim -t ~
+stow vim -t ~
+stow zshrc -t ~
+stow gitconfig -t ~
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
