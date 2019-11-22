5bae18a1d02955e4c77c8d4f9801095127f5c6b9
Zhansong Li <zhansong.li@ir.com>
2019-11-22 17:18:02 +1100

    stop stowing gitconfig

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ stow i3 -t ~
 stow nvim -t ~
 stow vim -t ~
 stow zshrc -t ~
-stow gitconfig -t ~
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
