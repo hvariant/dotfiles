83a5c8c09cce76d2407e08e725dbb9f5e7ba9de9
Zhansong Li <lizhansong@hvariant.com>
2020-02-28 21:32:24 +1100

    add tmux conf to stow

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ stow i3 -t ~
 stow nvim -t ~
 stow vim -t ~
 stow zshrc -t ~
+stow tmux -t ~
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
