66a13c9188d05a5b3ef339269078dc57ac028200
Jason Li <jason.li@plenti.com.au>
2020-11-25 09:03:10 +1100

    add emacs configuration

---
+++ emacs/init.el
@@ -0,0 +1,93 @@
+(require 'package)
+ 
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; https://github.com/emacs-evil/evil-collection
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+(use-package org
+  :ensure t)
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+(use-package helm
+  :ensure t
+  :config
+  (helm-mode 1)
+  (global-set-key (kbd "M-x") #'helm-M-x)
+  (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+  (global-set-key (kbd "C-x C-f") #'helm-find-files)
+  (global-set-key (kbd "C-c o") #'helm-occur))
+(use-package ag
+  :ensure t)
+
+;; https://www.sandeepnambiar.com/my-minimal-emacs-setup/
+(use-package smartparens
+  :ensure t
+  :config
+  (progn
+    (require 'smartparens-config)
+    (smartparens-global-mode 1)
+    (show-paren-mode t)))
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode +1))
+
+;; https://stackoverflow.com/a/23715631
+(define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
+(define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
+(define-key evil-insert-state-map (kbd "C-u")
+  (lambda ()
+    (interactive)
+    (evil-delete (point-at-bol) (point))))
+
+(setq visible-bell 1)
+
+;; https://www.emacswiki.org/emacs/BackupDirectory#toc2
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+(setq evil-emacs-state-modes nil)
+(setq evil-insert-state-modes nil)
+(setq evil-motion-state-modes nil)
+
+;; https://www.sandeepnambiar.com/my-minimal-emacs-setup/
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(blink-cursor-mode -1)
+
+(global-hl-line-mode +1)
+(line-number-mode +1)
+(global-display-line-numbers-mode 1)
+(column-number-mode t)
+(size-indication-mode t)
+
+;; https://emacs.stackexchange.com/a/3008
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; don't polute init.el with custom variables
+(setq custom-file "~/.emacs.d/garbage.el")
+
+(load-theme 'leuven)
