ea986c5451aa34c7dea5fad217e0273091d4d6ff
Jason Li <jason.li@plenti.com.au>
2020-12-04 21:00:11 +1100

    make emacs on windows usable
    - use ivy and counsel instead of helm
    - use webdev snippet from reddit to set up ide like environment
    - open in terminal snippet from ergoemacs
    - neotree

--- emacs/init.el
+++ emacs/init.el
@@ -1,10 +1,9 @@
 (require 'package)
+(package-initialize)
  
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-
-(package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -24,23 +23,39 @@
   ;; https://www.reddit.com/r/spacemacs/comments/6p3w0l/making_q_not_kill_emacs/
   (evil-ex-define-cmd "q" 'kill-this-buffer)
   (evil-ex-define-cmd "quit" 'evil-quit))
+
 (use-package org
   :ensure t)
-(use-package evil-collection
-  :after evil
+
+(use-package evil-org
+  :ensure t
+  :after (org evil)
+  :config
+  (add-hook 'org-mode-hook 'evil-org-mode)
+  (add-hook 'evil-org-mode-hook
+            (lambda ()
+              (evil-org-set-key-theme)))
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
+(use-package counsel
   :ensure t
   :config
-  (evil-collection-init))
-(use-package helm
-  :ensure t
-  :config
-  (helm-mode 1)
-  (global-set-key (kbd "M-x") #'helm-M-x)
-  (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
-  (global-set-key (kbd "C-x C-f") #'helm-find-files)
-  (global-set-key (kbd "C-c o") #'helm-occur))
-(use-package ag
+  (ivy-mode 1)
+  (counsel-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-count-format "(%d/%d) ")
+  (global-set-key (kbd "C-c k") 'counsel-rg))
+
+(use-package rg
   :ensure t)
+
+(use-package projectile
+  :ensure t
+  :config
+  (projectile-mode +1)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
 ;; https://www.sandeepnambiar.com/my-minimal-emacs-setup/
 (use-package smartparens
@@ -54,6 +69,106 @@
   :ensure t
   :config
   (which-key-mode +1))
+
+(use-package neotree
+  :ensure t
+  :config
+  (global-set-key [f8] 'neotree-toggle)
+  ;; https://www.emacswiki.org/emacs/NeoTree
+  (add-hook 'neotree-mode-hook
+          (lambda ()
+          (define-key evil-normal-state-local-map (kbd "TAB") 'neotree-enter)
+          (define-key evil-normal-state-local-map (kbd "SPC") 'neotree-quick-look)
+          (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+          (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)
+          (define-key evil-normal-state-local-map (kbd "g") 'neotree-refresh)
+          (define-key evil-normal-state-local-map (kbd "n") 'neotree-next-line)
+          (define-key evil-normal-state-local-map (kbd "p") 'neotree-previous-line)
+          (define-key evil-normal-state-local-map (kbd "A") 'neotree-stretch-toggle)
+          (define-key evil-normal-state-local-map (kbd "H") 'neotree-hidden-file-toggle)))
+  (setq neo-window-width 40)
+  (setq neo-smart-open t))
+
+;; webdev snippet from reddit
+(use-package flycheck
+  :ensure t
+  :config
+  (add-hook 'typescript-mode-hook 'flycheck-mode))
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+(use-package company
+  :ensure t
+  :config
+  (setq company-show-numbers t)
+  (setq company-tooltip-align-annotations t)
+  ;; invert the navigation direction if the the completion popup-isearch-match
+  ;; is displayed on top (happens near the bottom of windows)
+  (setq company-tooltip-flip-when-above t)
+  (global-company-mode))
+
+(use-package company-quickhelp
+  :ensure t
+  :init
+  (company-quickhelp-mode 1)
+  (use-package pos-tip
+    :ensure t))
+
+(use-package web-mode
+  :ensure t
+  :mode (("\\.html?\\'" . web-mode)
+         ("\\.tsx\\'" . web-mode)
+         ("\\.jsx\\'" . web-mode))
+  :config
+  (setq web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2
+        web-mode-block-padding 2
+        web-mode-comment-style 2
+        web-mode-enable-css-colorization t
+        web-mode-enable-auto-pairing t
+        web-mode-enable-comment-keywords t
+        web-mode-enable-current-element-highlight t
+        web-mode-enable-auto-indentation nil
+        )
+  (add-hook 'web-mode-hook
+            (lambda ()
+              (when (string-equal "tsx" (file-name-extension buffer-file-name))
+                (setup-tide-mode))))
+  ;; enable typescript-tslint checker
+  (flycheck-add-mode 'typescript-tslint 'web-mode))
+
+(use-package typescript-mode
+  :ensure t
+  :config
+  (setq typescript-indent-level 2)
+  (add-hook 'typescript-mode #'subword-mode))
+
+(use-package tide
+  :init
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)))
+
+(use-package css-mode
+  :config
+  (setq css-indent-offset 2))
+
+(use-package prettier-js
+  :config
+  (add-hook 'js2-mode-hook 'prettier-js-mode)
+  (add-hook 'web-mode-hook 'prettier-js-mode))
 
 ;; https://stackoverflow.com/a/23715631
 (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
@@ -70,6 +185,9 @@
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+;; https://stackoverflow.com/a/12974060
+(setq create-lockfiles nil)
 
 (setq evil-emacs-state-modes nil)
 (setq evil-insert-state-modes nil)
@@ -93,4 +211,23 @@
 ;; don't polute init.el with custom variables
 (setq custom-file "~/.emacs.d/garbage.el")
 
+;; no tabs !!!
+(setq-default indent-tabs-mode nil)
+
 (load-theme 'leuven)
+
+;; http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html
+(defun open-in-terminal ()
+  (interactive)
+  (cond
+   ((string-equal system-type "windows-nt")
+    (let ((process-connection-type nil))
+      (start-process "" nil "powershell" "start-process" "powershell"  "-workingDirectory" default-directory)))
+   ((string-equal system-type "darwin")
+    (shell-command (concat "open -a terminal " (shell-quote-argument (expand-file-name default-directory )))))
+   ((string-equal system-type "gnu/linux")
+    (let ((process-connection-type nil))
+      (start-process "" nil "x-terminal-emulator"
+                     (concat "--working-directory=" default-directory))))))
+
+(global-set-key (kbd "C-c t") 'open-in-terminal)
